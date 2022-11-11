9a58b20587e67fdd300958ec71e31482a7e958e3
Zuzana K <zk0433@gmail.com>
2022-11-11 09:55:50 +0000

    Add React

---
+++ shareme_frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="text-3xl font-bold underline">
+                Hello World!
+            </header>
+        </div>
+    );
+}
+
+export default App;
