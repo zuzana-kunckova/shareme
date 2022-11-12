406685de4fb2acef579bd587642a3238e36dc93d
Zuzana K <zk0433@gmail.com>
2022-11-12 23:33:35 +0000

    Sidebar done, needs more work but ok for now

--- shareme_frontend/src/components/Sidebar.jsx
+++ shareme_frontend/src/components/Sidebar.jsx
@@ -1,8 +1,74 @@
 import React from 'react'
+import { NavLink, Link } from 'react-router-dom'
+import { RiHomeFill } from 'react-icons/ri'
+import { IoIosArrowForward } from 'react-icons/io'
 
-const Sidebar = () => {
+import logo from '../assets/logo.png'
+
+const isNotActiveStyle = 'mt-2 flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize'
+const isActiveStyle = 'mt-2 flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize'
+
+const categories = [
+    { name: 'Animals' },
+    { name: 'Wallpaper' },
+    { name: 'Photography' },
+    { name: 'Gaming' },
+    { name: 'Coding' },
+]
+
+const Sidebar = ({ user, closeToggle }) => {
+    const handleCloseSidebar = () => {
+        if (closeToggle) closeToggle(false)
+    }
     return (
-        <div>Sidebar</div>
+        <div className='flex flex-col justify-between h-full overflow-y-scroll bg-white hide-scrollbar'>
+            <div className='flex flex-col'>
+                {/* <Link
+                    to='/'
+                    className='flex gap-2 px-5 pt-1 my-6'
+                    onClick={handleCloseSidebar}
+                > */}
+                <img src={logo} alt="logo" className='w-32 mt-3 mb-5 ml-3' />
+                {/* </Link> */}
+
+                <div className='flex flex-col gap-5'>
+                    <NavLink
+                        to='/'
+                        className={({ isActive }) => isActive ? isActiveStyle : isNotActiveStyle}
+                        onClick={handleCloseSidebar}
+                    >
+                        <RiHomeFill />
+                        Home
+                    </NavLink>
+
+                    <h3 className='px-5 mt-2 text-base text-left 2xl:text-xl'>Discover categories</h3>
+
+                    {categories.map((category) => (
+                        <NavLink
+                            to={`/category/${category.name}`}
+                            className={({ isActive }) => isActive ? isActiveStyle : isNotActiveStyle}
+                            onClick={handleCloseSidebar}
+                            key={category.name}
+                        >
+                            {category.name}
+                        </NavLink>
+                    ))}
+                </div>
+            </div>
+
+            {user && (
+                <Link
+                    to={`user-profile/${user._id}`}
+                    className='flex items-center gap-2 p-3 bg-white'
+                    onClick={handleCloseSidebar}
+                >
+                    <img src={user.image} alt="User image"
+                        className='w-10 h-10 rounded-full '
+                    />
+                    <p>{user.userName}</p>
+                </Link>
+            )}
+        </div>
     )
 }
 
--- shareme_frontend/src/container/Home.jsx
+++ shareme_frontend/src/container/Home.jsx
@@ -13,8 +13,7 @@ const Home = () => {
     const [toggleSidebar, setToggleSidebar] = useState(false);
     const [user, setUser] = useState(null)
     const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear()
-
-    console.log(userInfo)
+    const scrollRef = useRef(null)
 
     useEffect(() => {
         const query = userQuery(userInfo?.sub);
@@ -25,30 +24,36 @@ const Home = () => {
             })
     }, []);
 
+    useEffect(() => {
+        scrollRef.current.scrollTo(0, 0)
+    })
+
     return (
         <div className='flex flex-col h-screen duration-75 ease-out bg-gray-50 md:flex-row transition-height'>
-            <div className='flex-initial hidden h-screen md:fle'>
+            <div className='hidden h-screen md:flex'>
                 <Sidebar user={user && user} />
             </div>
 
             <div className='flex flex-row md:hidden'>
-                <HiMenu
-                    fontSize={40}
-                    className="cursor-pointer"
-                    onClick={() => setToggleSidebar(true)}
-                />
+                <div className='flex flex-row items-center justify-between w-full p-2 shadow-md'>
+                    <HiMenu
+                        fontSize={40}
+                        className="cursor-pointer"
+                        onClick={() => setToggleSidebar(true)}
+                    />
 
-                <Link to="/">
-                    <img src={logo} alt="logo" className='w-28' />
-                </Link>
+                    <Link to="/">
+                        <img src={logo} alt="logo" className='w-28' />
+                    </Link>
 
-                <Link to={`user-profile/${user?._id}`}>
-                    <img src={user?.image} alt="logo" className='w-8' referrerPolicy="no-referrer" />
-                </Link>
+                    <Link to={`user-profile/${user?._id}`}>
+                        <img src={user?.image} alt="logo" className='w-9' referrerPolicy="no-referrer" />
+                    </Link>
+                </div>
 
                 {toggleSidebar && (
-                    <div className='fixed z-10 w-4/5 h-screen overflow-y-auto bg-white shadow-md animate-slide-in'>
-                        <div className='flex items-center justify-end w-full p-2 aboslute'>
+                    <div className='fixed z-10 w-4/5 h-screen overflow-y-auto duration-300 bg-white shadow-md animate-in slide-in-from-left'>
+                        <div className='absolute flex items-center justify-end w-full p-2 mt-1'>
                             <AiFillCloseCircle
                                 fontSize={30} className="cursor-pointer"
                                 onClick={() => setToggleSidebar(false)}
@@ -58,6 +63,13 @@ const Home = () => {
                         <Sidebar user={user && user} closeToggle={setToggleSidebar} />
                     </div>
                 )}
+            </div>
+
+            <div className='flex-1 h-screen pb-2 overflow-y-scrool' ref={scrollRef}>
+                <Routes>
+                    <Route path="/user-profile/:userId" element={<UserProfile />} />
+                    <Route path="/*" element={<Pins user={user && user} />} />
+                </Routes>
             </div>
         </div>
     )
